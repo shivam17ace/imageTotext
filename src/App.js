2bc09b25b78b4032b3646b9d8e7fdd5c81bdaf86
shivam <shivam17101@gmail.com>
2022-07-01 10:53:30 +0530

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState,useRef } from 'react';
+// import './App.css';
+// import Tesseract from 'tesseract.js';
 
+// function App() {
+//   const [getText, setText] = useState('');
+//   const [imagePath, setImagePath] = useState('');
+//   const [display, setDisplay] = useState(false)
+//   const handleOnChange = (e) =>{
+//     setImagePath(URL.createObjectURL(e.target.files[0]));
+//     console.log(URL.createObjectURL(e.target.files[0]))
+//   } 
+//   const handleClick = () =>{
+//     Tesseract.recognize(
+//       imagePath,'eng',
+//       {
+//         logger: m =>
+//         console.log(m)
+//       }
+//     )
+//     .catch((err)=>{console.log(err)})
+//     .then((res)=>{
+//       setText(res);
+//       setDisplay(true)
+//     })
+//   }
+//   return (
+//     <div className="App">
+//       { display
+//         ? 
+//           <div>
+//             <p>{JSON.stringify(getText)}</p>
+//           </div>
+//         :
+//           <div>
+//           <h1>hello</h1>
+//             <img src={imagePath} alt="logo" />
+//             <h3>Text From Image</h3>
+//             <input type="file" onChange={handleOnChange} />
+//             <button onClick={handleClick}>Convert Image To Text</button> 
+//           </div>
+//       }
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import { useEffect, useState } from "react";
+import { createWorker } from "tesseract.js";
+import "./App.css";
 function App() {
+  const [ocr, setOcr] = useState("");
+  const [imageData, setImageData] = useState(null);
+  const worker = createWorker({
+    logger: (m) => {
+      console.log(m);
+    },
+  });
+  const convertImageToText = async () => {
+    if (!imageData) return;
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const {
+      data: { text },
+    } = await worker.recognize(imageData);
+    setOcr(text);
+  };
+
+  useEffect(() => {
+    convertImageToText();
+  }, [imageData]);
+
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+    if(!file)return;
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const imageDataUri = reader.result;
+      console.log({ imageDataUri });
+      setImageData(imageDataUri);
+    };
+    reader.readAsDataURL(file);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <p>Choose an Image</p>
+        <input
+          type="file"
+          name=""
+          id=""
+          onChange={handleImageChange}
+          accept="image/*"
+        />
+      </div>
+      <div className="display-flex">
+        <img src={imageData} alt="" srcset="" />
+        <p>{ocr}</p>
+      </div>
     </div>
   );
 }
-
 export default App;
+// */
